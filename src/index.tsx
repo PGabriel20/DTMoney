cc7579889a0172d919e278d7f683525aa467cb94
PGabriel20 <bielporreca@gmail.com>
2021-07-11 11:40:00 -0300

    feat: creating post route on mirage

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,64 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 import { App } from './App';
 
 createServer({
+
+  //Setting up Mirage database
+  //Naming tables
+
+  models: {
+    transaction: Model,
+  },
+
+
+  //Saving initial data in api using seeds (optional)
+  seeds(server){
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Desenvolvimento de Website',
+          type: 'deposit',
+          category: 'Desenvolvimento',
+          amount: 10000,
+          createdAt: new Date('2021-04-10 08:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 200,
+          createdAt: new Date('2021-04-05 08:00:00'),
+        }
+      ]
+    });
+  },
+
   routes(){
     this.namespace = 'api';
 
     this.get('/transactions', ()=>{
-      return[
-        {
-          id: 1,
-          title: 'Transaction 1',
-          amount: 400,
-          type: 'deposit',
-          category: 'food',
-          createdAt: new Date()
-        }
-      ]
+      // return[
+      //   {
+      //     id: 1,
+      //     title: 'Transaction 1',
+      //     amount: 400,
+      //     type: 'deposit',
+      //     category: 'food',
+      //     createdAt: new Date()
+      //   }
+      // ]
+
+      return this.schema.all('transaction');
     });
 
     this.post('/transactions', (schema, request)=>{
       const data = JSON.parse(request.requestBody);
 
-      return data;
+      return schema.create('transaction', data);
     })
   }
 })
